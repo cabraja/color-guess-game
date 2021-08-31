89aec2e3ac3dfe81200e32ea016f9f2b54cd0cab
cabraja <cabrajamihajlo@gmail.com>
2021-08-31 17:57:34 +0200

    added score counter and made boxes clickable

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
-const wantedColor = document.getElementById('wanted-color');
-const wantedIndex = Math.floor(Math.random()*5);
+const wantedColorText = document.getElementById('wanted-color');
+const newButton = document.getElementById('new-colors');
+const scoreBox = document.getElementById('score-box');
+const failMessages = ['Nope','Bad Luck','Oopsie','Really?','No','Try Again','Nein'];
+let score = 0;
 
-const boxes = document.querySelectorAll('.color');
+const newColors = () => {
+    const wantedIndex = Math.floor(Math.random()*5);
 
-boxes.forEach((box,index) => {
+    const boxes = document.querySelectorAll('.color');
+
+    boxes.forEach((box,index) => {
 
     const num1 = Math.floor(Math.random() * 255);
     const num2 = Math.floor(Math.random() * 255);
@@ -12,6 +18,27 @@ boxes.forEach((box,index) => {
     box.style.backgroundColor=`rgb(${num1},${num2},${num3})`;
 
     if(index == wantedIndex){
-        wantedColor.innerHTML = `RGB(${num1},${num2},${num3})`;
+        wantedColorText.innerHTML = `RGB(${num1},${num2},${num3})`;
+        
     }
+
+    box.addEventListener('click',e => {
+        if(index == wantedIndex){
+            box.innerHTML='<h6>Correct</h6>';
+            score++;
+            scoreBox.innerHTML=`Score: ${score}`;
+        }else{
+            const message = failMessages[Math.floor(Math.random()*6)];
+
+            box.innerHTML=`<h6>${message}</h6>`;
+        }
+    })
+
+    
+
 })
+}
+
+newColors();
+
+newButton.addEventListener('click',newColors);
